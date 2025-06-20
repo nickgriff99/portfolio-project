591faca9e6fc0b1136f80ab9b98f90f5fe019332
Nicholas Griffiths <nichg999@gmail.com>
2025-06-19 20:28:44 -0600

    added carousel js file to project

---
+++ js/carousel.js
@@ -0,0 +1,9 @@
+const slides = document.querySelectorAll('.review-item');
+const buttons = document.querySelectorAll('.slide-ctrl-container button');
+
+let current = Math.floor(Math.random() * slides.length);
+let next = current < slides.length - 1 ? current + 1 : 0;
+let prev = current > 0 ? current - 1 : slides.length - 1;
+
+
+
