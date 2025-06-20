dec4c5e0ed211b669553475bd6d858f99091c534
Nicholas Griffiths <nichg999@gmail.com>
2025-06-19 20:48:49 -0600

    added logic to calculate carousel order/function

--- js/carousel.js
+++ js/carousel.js
@@ -5,5 +5,16 @@ let current = Math.floor(Math.random() * slides.length);
 let next = current < slides.length - 1 ? current + 1 : 0;
 let prev = current > 0 ? current - 1 : slides.length - 1;
 
+const goToNumber = (number) => {
+  current = number;
+  next = current < slides.length - 1 ? current + 1 : 0;
+  prev = current > 0 ? current - 1 : slides.length - 1;
+};
 
+const goToNext = () => { current < slides.length - 1 ? goToNumber(current + 1) : goToNumber(0) };
+const goToPrev = () => { current > 0 ? goToNumber(current - 1) : goToNumber(slides.length - 1)};
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => i === 0 ? goToPrev() : goToNext());
+};
 
